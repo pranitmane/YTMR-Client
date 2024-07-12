5476c5d49c05400cdf82529ddd32c626a282dab5
pranitmane <manepranit10@gmail.com>
2024-07-12 15:07:06 +0530

    fix styles

--- src/components/MediaControls.tsx
+++ src/components/MediaControls.tsx
@@ -1,17 +1,37 @@
-export function MediaControls({ pauseOrPlay, playNext, playPrevious }: {
+import { PlayIcon, PauseIcon, TrackNextIcon, TrackPreviousIcon, Half2Icon } from '@radix-ui/react-icons'
+
+export function MediaControls({ pauseOrPlay, playNext, playPrevious, isPlaying }: {
   pauseOrPlay: () => void,
   playNext: () => void,
-  playPrevious: () => void
+  playPrevious: () => void,
+  isPlaying: "playing" | "paused" | "loading" | "nothing"
 }) {
-  return <div className="flex flex-row gap-2 justify-center">
-    <button onClick={(e) => { e.preventDefault; playPrevious() }} className="bg-gray-200 border border-gray-500 p-2 rounded-md hover:bg-gray-300">
-      previous
-    </button>
-    <button onClick={(e) => { e.preventDefault; pauseOrPlay() }} className="bg-gray-200 border border-gray-500 p-2 rounded-md hover:bg-gray-300">
-      play/pause
-    </button>
-    <button onClick={(e) => { e.preventDefault; playNext() }} className="bg-gray-200 border border-gray-500 p-2 rounded-md hover:bg-gray-300">
-      next
-    </button>
-  </div>
+  const buttonClass = "bg-zinc-900/30 shadow-md p-5 rounded-full hover:bg-zinc-800/30";
+  const iconClass = "text-zinc-300 h-5 w-5";
+
+  return (
+    <div className="flex flex-1 flex-row gap-2 justify-center items-center">
+      <button onClick={playPrevious} className={buttonClass}>
+        <TrackPreviousIcon className={iconClass} />
+      </button>
+      {isPlaying === "loading" ? (
+        <button disabled className={`${buttonClass} p-8`}>
+          <Half2Icon className={`${iconClass} animate-spin`} />
+        </button>
+      ) : (
+        <button onClick={pauseOrPlay} className={`${buttonClass} p-8`}>
+          {isPlaying === "playing" ? (
+            <PauseIcon className={iconClass} />
+          ) : (
+            <PlayIcon className={iconClass} />
+          )}
+        </button>
+      )}
+      <button onClick={playNext} className={buttonClass}>
+        <TrackNextIcon className={iconClass} />
+      </button>
+    </div>
+  );
 }
+
+
